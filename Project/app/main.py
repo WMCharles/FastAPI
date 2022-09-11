d9e4bac13cce62f6f2b91069d7ddfca560f63a4e
WMCharles <charles.wafula@student.moringaschool.com>
2022-09-11 20:07:25 +0300

    add operation to retrieve single post

--- Project/app/main.py
+++ Project/app/main.py
@@ -1,11 +1,17 @@
 from random import randrange
 from typing import Union
-from fastapi import FastAPI, status
+from fastapi import FastAPI, status, HTTPException
 from pydantic import BaseModel
 
 app = FastAPI()
 
 my_posts = [{"title":"First Post", "content":"This is the first post", "id":1}, {"title":"Second Post", "content":"This is the second post", "id":2}]
+
+# Function to get single post
+def get_post(id):
+    for p in my_posts:
+        if p["id"] == id:
+            return p
 
 class Post(BaseModel):
     title:str
@@ -16,10 +22,18 @@ class Post(BaseModel):
 def root():
     return {"message":"Hello Welcome"}
 
-# Retrieving posts
+# Retrieving all posts
 @app.get("/posts")
 def get_posts():
     return {"message": my_posts}
+
+# Retrieving single post
+@app.get("/posts/{id}")
+def get_post(id: int):
+    post = get_post(id)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with {id} not found!")
+    return {"post": post}
 
 # Create Post
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
