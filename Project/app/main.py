fccff4d2fef17ce1e3902854c593eaa869476327
WMCharles <charles.wafula@student.moringaschool.com>
2022-09-11 19:48:03 +0300

    add path operation -post

--- Project/app/main.py
+++ Project/app/main.py
@@ -1,8 +1,15 @@
-from fastapi import FastAPI
+from random import randrange
+from typing import Union
+from fastapi import FastAPI, status
+from pydantic import BaseModel
 
 app = FastAPI()
 
 my_posts = [{"title":"First Post", "content":"This is the first post", "id":1}, {"title":"Second Post", "content":"This is the second post", "id":2}]
+
+class Post(BaseModel):
+    title:str
+    content: Union[str, None] = None
 
 # Base url
 @app.get("/")
@@ -13,3 +20,12 @@ def root():
 @app.get("/posts")
 def get_posts():
     return {"message": my_posts}
+
+# Create Post
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
+def create_post(post: Post):
+    post_dict = post.dict()
+    post_dict["id"] = randrange(0,100)
+    my_posts.append(post_dict)
+    print(post_dict)
+    return {"message": post_dict}
